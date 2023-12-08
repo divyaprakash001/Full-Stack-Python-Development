42aab690d3b1b24315b1fdf5b0d152828101eac2
Divya Prakash <divyaprakashara09@gmail.com>
2023-12-08 18:04:13 +0530

    added

---
+++ Python Programs/mathModule8dec/math1.py
@@ -0,0 +1,9 @@
+# a = -10.0
+# print(abs(a))
+a = [1,4,34,6,34,6,35]
+print(max(1,9,6))
+print(max(a))
+print(min(a))
+
+# print(ascii('\n')) #need to understand later
+
---
+++ Python Programs/mathModule8dec/math2.py
@@ -0,0 +1,37 @@
+import math as m
+# print(m.pow(2,3))
+# print(m.pow(2.34,3))
+# print(2.5**2)
+# print(2**2)
+
+# print(m.ceil(2.1))
+# print(m.ceil(2.7))
+# print(m.ceil(2.5))
+# print(m.ceil(2.0))
+# print(m.ceil(-2.6)) 
+
+# print(m.floor(2.1))
+# print(m.floor(2.7))
+# print(m.floor(2.5))
+# print(m.floor(2.0))
+# print(m.floor(-2.6))
+
+# print(round(2.3))
+# print(m.trunc(-2.3))
+# print(m.factorial(5))
+
+# print(m.degrees(1))
+# print(m.radians(57.29577951308232))
+
+# print(m.exp(5))
+
+# print(m.fabs(-23))
+
+# print(m.fmod(5,3))
+# print(m.fmod(5.3,3))
+# print(5%2)
+# print(5.5%2)
+# print(m.fsum([3,4,5,6]))
+
+print(m.gcd(34,765))
+print(m.gcd(9,12))
