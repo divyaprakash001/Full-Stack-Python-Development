6814501c997561615b5f0b134af8ab09f2a6e0c8
Divya Prakash <divyaprakashara09@gmail.com>
2023-12-10 06:18:11 +0530

    linear search binary search first question added

--- PythonDSAJovien/linearSearchAlgo.py
+++ PythonDSAJovien/linearSearchAlgo.py
@@ -12,12 +12,15 @@ def locate_cards(cards,query):
           return -1
         
     
-print(locate_cards([1,2,4,3,6,4,8],2))
-print(locate_cards([1,2,4,3,2,6,4,8],2))
-print(locate_cards([1,2,4,3,6,4,8],-2))
+print(locate_cards([8,7,4,3,2,1],2))
+print(locate_cards([8,7,4,3,2,1],2))
+print(locate_cards([8,7,4,3,2,1],2))
 # print(locate_cards([],-2))  #encounter error with this solution when the cards are empty
 '''
 
+
+'''
+# brute force solution
 def locate_cards(cards,query):
     position=0
     while position<len(cards):
@@ -26,7 +29,68 @@ def locate_cards(cards,query):
         position += 1
     return -1
         
-print(locate_cards([1,2,4,3,6,4,8],2))
-print(locate_cards([1,2,4,3,2,6,4,8],2))
-print(locate_cards([1,2,4,3,6,4,8],-2))
-print(locate_cards([],-2))
+
+print(locate_cards([8,7,4,3,2,1],2))
+print(locate_cards([8,7,4,3,2,1],1))
+print(locate_cards([8,7,4,3,2,1],-2))
+print(locate_cards([],-2))  
+'''
+'''
+# applying bineary search algorithm for efficiency
+
+def locate_cards(cards,query):
+    lo,hi = 0,len(cards)-1
+
+    while lo<=hi:
+        mid = (lo+hi)//2
+        mid_number = cards[mid]
+
+        print('lo',lo,'hi',hi,'mid',mid,'mid_number',mid_number)
+
+        if mid_number ==  query:
+            return mid
+        elif mid_number < query:
+            hi = mid-1
+        elif mid_number > query:
+            lo = mid+1
+        
+    return -1
+
+# print(locate_cards([199,45,76,58,35,24,12,10,8,7,4,3,2,1],2))
+# print(locate_cards([8,7,4,3,2,1],1))
+# print(locate_cards([8,7,4,3,2,1],-2))
+# print(locate_cards([],-2))  
+print(locate_cards([8,8,8,6,6,6,6,6,6,6,6,6,5,5,5,2,2,2,1,1,1,],6))  #test cases failed here as we have to get the first occurence of the queries element
+'''
+
+def test_location(cards,query,mid):
+    mid_number = cards[mid]
+    print('mid',mid,'mid_number',mid_number)
+
+    if mid_number == query:
+        if mid-1>0 and cards[mid-1]==query:
+            return 'left'
+        else:
+            return 'found'
+    elif mid_number<query:
+        return 'left'
+    else:
+        return 'right'
+    
+def locate_cards(cards,query):
+    lo, hi = 0, len(cards)-1
+    while lo < hi:
+        print('lo',lo,'hi',hi)
+        mid = (lo+hi)//2
+        result = test_location(cards,query,mid)
+
+        if result == 'found':
+            return mid
+        elif result == 'left':
+             hi = mid-1
+        elif result == 'right':
+            lo = mid+1
+    return -1
+
+
+print(locate_cards([8,8,8,6,6,6,6,6,6,6,6,6,5,5,5,2,2,2,1,1,1,],6))  #test cases failed here as we have to get the first occurence of the queries element
