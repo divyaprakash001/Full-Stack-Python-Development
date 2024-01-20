d967a0b238682f1fed31e6c344d1a640dcc91300
Divya Prakash <divyaprakashara09@gmail.com>
2024-01-20 17:40:36 +0530

    added new django project studybud

---
+++ django_tut/studybud/base/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+rooms= [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developer'}
+]
+
+def home(request):
+    # return HttpResponse('Home page')
+    context = {'rooms':rooms}
+    return render(request,"base/home.html",context)
+
+def room(request):
+    # return HttpResponse('room')
+    return render(request,"base/room.html",{'rooms':rooms})
