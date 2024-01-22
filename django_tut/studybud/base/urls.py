ed8f95c11297c24ec10482027c8a2f66164d4f54
Divya Prakash <divyaprakashara09@gmail.com>
2024-01-22 06:14:44 +0530

    added some files in django_tut

--- django_tut/studybud/base/urls.py
+++ django_tut/studybud/base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home page'),
-    path('room/', views.room, name='room page'),
+    path('room/<str:pk>/', views.room, name='room'),
     
 ]
