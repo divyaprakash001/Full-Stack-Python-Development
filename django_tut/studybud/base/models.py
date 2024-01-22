cf0c0f59d5d9fe64bf081531013952324d1a0c8d
Divya Prakash <divyaprakashara09@gmail.com>
2024-01-22 18:28:26 +0530

    modification in django project

--- django_tut/studybud/base/models.py
+++ django_tut/studybud/base/models.py
@@ -13,3 +13,15 @@ class Room(models.Model):
 
     def __str__(self):
         return str(self.name)
+    
+
+class Message(models.Model):
+    # user
+    room = models.ForeignKey(Room, on_delete = models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
--- django_tut/studybud/base/views.py
+++ django_tut/studybud/base/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
-rooms= [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Design with me'},
-    {'id':3, 'name':'Frontend developer'}
-]
+# rooms= [
+#     {'id':1, 'name':'Lets learn python!'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Frontend developer'}
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     # return HttpResponse('Home page')
     context = {'rooms':rooms}
     return render(request,"base/home.html",context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+   
     context = {'room':room}
     return render(request,"base/room.html",context)
