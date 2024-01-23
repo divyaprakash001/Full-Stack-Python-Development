88c696f6efc0dbbce307bc9f3c9f67b3ad5b49b7
Divya Prakash <divyaprakashara09@gmail.com>
2024-01-23 13:43:36 +0530

    modification in django project

--- django_tut/studybud/base/views.py
+++ django_tut/studybud/base/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate,login,logout
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import is_valid_path
 from .models import Room,Topic
 from .forms import RoomForm
-from django.contrib.auth.models import User
 
 # Create your views here.
 # rooms= [
@@ -17,12 +19,13 @@ from django.contrib.auth.models import User
 
 
 def loginPage(request):
+    page='login'
 
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -39,13 +42,30 @@ def loginPage(request):
             messages.error(request,"Username and password does not exist")
             
 
-    context = {}
+    context = {'page':page}
     return render(request,'base/login_register.html',context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'An error occured during registration')
+
+
+    return render(request,'base/login_register.html',{'form':form})
 
 
 def home(request):
