d5bb2a55b7e5b81e0b1b9d307b8907b19b675465
Divya Prakash <divyaprakashara09@gmail.com>
2024-01-23 23:02:28 +0530

    some modification in studybud

--- django_tut/studybud/base/views.py
+++ django_tut/studybud/base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import is_valid_path
-from .models import Room,Topic
+from .models import Room,Topic,Message
 from .forms import RoomForm
 
 # Create your views here.
@@ -84,8 +84,16 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-   
-    context = {'room':room}
+    room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room',pk = room.id)
+    context = {'room':room,'messages':room_messages}
     return render(request,"base/room.html",context)
 
 @login_required(login_url='/login')
