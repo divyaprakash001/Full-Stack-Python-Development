9aea0d70b3f5d32ebecc1669106b901e589123e7
Divya Prakash <divyaprakashara09@gmail.com>
2024-01-24 15:58:25 +0530

    aaded new prject

--- django_tut/Raysproject/raysonline/views.py
+++ django_tut/Raysproject/raysonline/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+    # return HttpResponse('<h1>This is the home page</h1>')
+
+def contactus(request):
+    return HttpResponse('<h1>This is the contact page</h1>')
