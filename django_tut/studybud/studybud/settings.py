c713e02c9e7c03e65dbc10fa196af0afd261bbd6
Divya Prakash <95012051+divyaprakash001@users.noreply.github.com>
2024-01-31 15:58:42 +0530

    Update settings.py

--- django_tut/studybud/studybud/settings.py
+++ django_tut/studybud/studybud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n#=94436sz(-uq6m_mw$1d7nyy^nwv(4im%(i8n^+%_$u+cy1+'
+SECRET_KEY = 'security reason'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
